fe0510d6e0272a9e66d57c4320e5575b64001a2a
ChrisZili <kiklop421@gmail.com>
2022-02-23 15:18:02 +0100

    cyklus s vypisom

--- src/main/java/UlohaCvicenie1.java
+++ src/main/java/UlohaCvicenie1.java
@@ -3,6 +3,9 @@ public class UlohaCvicenie1 {
 
     helloWord();
     System.out.println(porovnanie(2,1));
+    for(int i = 0; i < 10;i++) {
+        System.out.println(String.format("Funkcia bola vypisana %d", i+1));
+    }
 
     }
     public static void helloWord(){
