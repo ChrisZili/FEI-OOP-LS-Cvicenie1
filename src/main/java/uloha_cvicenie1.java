d2ace6ed240da5d1da59a5b97b31061af0d9b0a8
ChrisZili <kiklop421@gmail.com>
2022-02-23 12:27:05 +0100

    uprava konvencii

--- src/main/java/uloha_cvicenie1.java
+++ src/main/java/uloha_cvicenie1.java
@@ -1,16 +1,16 @@
 public class uloha_cvicenie1 {
     public static void main(String[] args){
 
-    HelloWord();
-    System.out.println(Porovnanie(2,1));
+    helloWord();
+    System.out.println(porovnanie(2,1));
 
     }
-    public static void HelloWord(){
+    public static void helloWord(){
         System.out.println("Hello world!");
 
     }
 
-    public static int Porovnanie(int a,int b){
+    public static int porovnanie(int a,int b){
         if (a > b)
             return 1;
         else if (a == b)
